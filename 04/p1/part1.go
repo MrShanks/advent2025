1514bd7b8925998d14b045d770a394d9f3ca68bf
MrShanks <simonestaffoli@gmail.com>
2025-12-04 14:28:30 +0100

    add stuff

--- 04/p1/part1.go
+++ 04/p1/part1.go
@@ -1,8 +1,6 @@
 package p1
 
 import (
-	"fmt"
-
 	"github.com/MrShanks/advent2025/utils"
 )
 
@@ -10,11 +8,35 @@ func Solve(filepath string) int {
 	f, scanner := utils.ReadInput(filepath)
 	defer f.Close()
 
-	counter := 0
+	var matrix [][]string
 	for scanner.Scan() {
 		raw := scanner.Text()
+		var row []string
+		for _, r := range raw {
+			row = append(row, string(r))
+		}
+		matrix = append(matrix, row)
+	}
 
-		fmt.Println(raw)
+	rows := len(matrix)
+	cols := len(matrix[0])
+	counter := 0
+
+	for r := range rows {
+		for c := range cols {
+			neighbour := 0
+			for _, dr := range []int{-1, 0, 1} {
+				for _, dc := range []int{-1, 0, 1} {
+					posr, posc := r+dr, c+dc
+					if posr >= 0 && posr < rows && posc >= 0 && posc < cols && matrix[posr][posc] == "@" {
+						neighbour += 1
+					}
+				}
+			}
+			if matrix[r][c] == "@" && neighbour < 5 {
+				counter += 1
+			}
+		}
 	}
 
 	return counter
--- 04/p2/part2.go
+++ 04/p2/part2.go
@@ -1,20 +1,62 @@
 package p2
 
 import (
-	"fmt"
-
 	"github.com/MrShanks/advent2025/utils"
 )
+
+func calculate(matrix [][]string) int {
+	rows := len(matrix)
+	cols := len(matrix[0])
+	counter := 0
+
+	changed := true
+
+	for changed {
+		changed = false
+
+		//loop trough each cell
+		for r := range rows {
+			for c := range cols {
+
+				neighbour := 0
+				//loop trough each neighbour
+				for _, dr := range []int{-1, 0, 1} {
+					for _, dc := range []int{-1, 0, 1} {
+						posr, posc := r+dr, c+dc
+
+						//check wew are within boundaries and the cell is a rollpaper
+						if posr >= 0 && posr < rows && posc >= 0 && posc < cols && matrix[posr][posc] == "@" {
+							neighbour += 1
+						}
+					}
+				}
+				//if we have less than 5 neighbours then we can remove the paperoll
+				if matrix[r][c] == "@" && neighbour < 5 {
+					changed = true
+					counter += 1
+					matrix[r][c] = "."
+				}
+			}
+		}
+	}
+	return counter
+}
 
 func Solve(filepath string) int {
 	f, scanner := utils.ReadInput(filepath)
 	defer f.Close()
 
-	counter := 0
+	var matrix [][]string
 	for scanner.Scan() {
 		raw := scanner.Text()
-		fmt.Println(raw)
+		var row []string
+		for _, r := range raw {
+			row = append(row, string(r))
+		}
+		matrix = append(matrix, row)
 	}
 
-	return counter
+	tot := calculate(matrix)
+
+	return tot
 }
