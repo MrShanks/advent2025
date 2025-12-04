7d768345dce8cfa2880abe0bd6edd78bcfd95382
MrShanks <simonestaffoli@gmail.com>
2025-12-04 14:56:20 +0100

    refactor a little bit

--- 04/p1/part1.go
+++ 04/p1/part1.go
@@ -4,6 +4,32 @@ import (
 	"github.com/MrShanks/advent2025/utils"
 )
 
+func calculate(matrix [][]string) int {
+	rows := len(matrix)
+	cols := len(matrix[0])
+	counter := 0
+
+	for r := range rows {
+		for c := range cols {
+
+			neighbour := 0
+			for _, dr := range []int{-1, 0, 1} {
+				for _, dc := range []int{-1, 0, 1} {
+
+					posr, posc := r+dr, c+dc
+					if posr >= 0 && posr < rows && posc >= 0 && posc < cols && matrix[posr][posc] == "@" {
+						neighbour += 1
+					}
+				}
+			}
+			if matrix[r][c] == "@" && neighbour < 5 {
+				counter += 1
+			}
+		}
+	}
+	return counter
+}
+
 func Solve(filepath string) int {
 	f, scanner := utils.ReadInput(filepath)
 	defer f.Close()
@@ -18,26 +44,5 @@ func Solve(filepath string) int {
 		matrix = append(matrix, row)
 	}
 
-	rows := len(matrix)
-	cols := len(matrix[0])
-	counter := 0
-
-	for r := range rows {
-		for c := range cols {
-			neighbour := 0
-			for _, dr := range []int{-1, 0, 1} {
-				for _, dc := range []int{-1, 0, 1} {
-					posr, posc := r+dr, c+dc
-					if posr >= 0 && posr < rows && posc >= 0 && posc < cols && matrix[posr][posc] == "@" {
-						neighbour += 1
-					}
-				}
-			}
-			if matrix[r][c] == "@" && neighbour < 5 {
-				counter += 1
-			}
-		}
-	}
-
-	return counter
+	return calculate(matrix)
 }
