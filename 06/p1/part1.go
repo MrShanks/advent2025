b95792e93950888ff1e97c40d0eb48027e45db2e
MrShanks <simonestaffoli@gmail.com>
2025-12-07 14:50:12 +0100

    add 06

--- 06/p1/part1.go
+++ 06/p1/part1.go
@@ -2,20 +2,75 @@ package p1
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/MrShanks/advent2025/utils"
 )
+
+func calculate(cols [][]int, operand []string) int {
+
+	tot := make([]int, 0)
+
+	for i := range len(cols[0]) {
+		switch operand[i] {
+		case "+":
+			{
+				count := 0
+				for j := range len(cols) {
+					count += cols[j][i]
+				}
+				tot = append(tot, count)
+			}
+		case "*":
+			{
+				count := 1
+				for j := range len(cols) {
+					count *= cols[j][i]
+				}
+				tot = append(tot, count)
+			}
+		}
+	}
+
+	count := 0
+	for _, e := range tot {
+		count += e
+	}
+
+	return count
+}
 
 func Solve(filepath string) int {
 	f, scanner := utils.ReadInput(filepath)
 	defer f.Close()
 
-	counter := 0
+	cols := make([][]string, 0)
 	for scanner.Scan() {
 		raw := scanner.Text()
 
-		fmt.Println(raw)
+		numbers := strings.Fields(raw)
+
+		cols = append(cols, numbers)
 	}
 
-	return counter
+	//remove operands
+	operands := cols[len(cols)-1]
+	cols = slices.Delete(cols, len(cols)-1, len(cols))
+
+	columns := [][]int{}
+	for _, row := range cols {
+		numbers := []int{}
+		for _, str := range row {
+			num, err := strconv.Atoi(str)
+			if err != nil {
+				fmt.Println("failed to conver:", err)
+				continue
+			}
+			numbers = append(numbers, num)
+		}
+		columns = append(columns, numbers)
+	}
+	return calculate(columns, operands)
 }
