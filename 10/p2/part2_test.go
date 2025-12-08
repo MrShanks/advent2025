3225d0ac2d4c6f9f4e95590aef0054a2c76d6747
MrShanks <simonestaffoli@gmail.com>
2025-12-08 22:52:42 +0100

    prepare for day 09

---
+++ 10/p2/part2_test.go
@@ -0,0 +1,16 @@
+package p2
+
+import (
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	t.Run("Solution", func(t *testing.T) {
+		got := Solve("../input_test.txt")
+		expected := -1
+
+		if got != expected {
+			t.Errorf("Expected: %d\nGot %d\n", expected, got)
+		}
+	})
+}
